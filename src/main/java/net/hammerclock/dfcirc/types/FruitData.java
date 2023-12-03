80103a91f989193655f1699c670a30a6a4d01c73
rathmerdominik <rathmerdominik@outlook.de>
2023-12-03 17:52:14 +0100

    refactor: Refactor the entire Project going away from the thread based approach and switching over to an event driven approach

--- src/main/java/net/hammerclock/dfcirc/types/FruitData.java
+++ src/main/java/net/hammerclock/dfcirc/types/FruitData.java
@@ -4,32 +4,32 @@ import java.util.Optional;
 import xyz.pixelatedw.mineminenomi.api.OneFruitEntry;
 
 public class FruitData {
-   public final String devilFruitName;
-   public final String devilFruitKey;
-   public final Optional<OneFruitEntry.Status> devilFruitStatus;
-   public final TierBox devilFruitTier;
+	public final String devilFruitName;
+	public final String devilFruitKey;
+	public final Optional<OneFruitEntry.Status> devilFruitStatus;
+	public final TierBox devilFruitTier;
 
-   public FruitData(String devilFruitName, String devilFruitKey, OneFruitEntry.Status devilFruitStatus,
-         TierBox devilFruitTier) {
-      this.devilFruitName = devilFruitName;
-      this.devilFruitKey = devilFruitKey;
-      this.devilFruitStatus = Optional.ofNullable(devilFruitStatus);
-      this.devilFruitTier = devilFruitTier;
-   }
+	public FruitData(String devilFruitName, String devilFruitKey, OneFruitEntry.Status devilFruitStatus,
+			TierBox devilFruitTier) {
+		this.devilFruitName = devilFruitName;
+		this.devilFruitKey = devilFruitKey;
+		this.devilFruitStatus = Optional.ofNullable(devilFruitStatus);
+		this.devilFruitTier = devilFruitTier;
+	}
 
-   public String getDevilFruitName() {
-      return this.devilFruitName;
-   }
+	public String getDevilFruitName() {
+		return this.devilFruitName;
+	}
 
-   public String getDevilFruitKey() {
-      return this.devilFruitKey;
-   }
+	public String getDevilFruitKey() {
+		return this.devilFruitKey;
+	}
 
-   public Optional<OneFruitEntry.Status> getDevilFruitStatus() {
-      return this.devilFruitStatus;
-   }
+	public Optional<OneFruitEntry.Status> getDevilFruitStatus() {
+		return this.devilFruitStatus;
+	}
 
-   public TierBox getDevilFruitTier() {
-      return this.devilFruitTier;
-   }
+	public TierBox getDevilFruitTier() {
+		return this.devilFruitTier;
+	}
 }
